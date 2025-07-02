7d096a1bb33a1ff7df14faca1b914bfd2563de5e
sr0116 <kysy0116@gmail.com>
2025-07-02 20:57:53 +0900

    클래스 정보 매매 xml 추가 및 테스트 연동

--- src/test/java/mapper/ClassInfoMapperTest.java
+++ src/test/java/mapper/ClassInfoMapperTest.java
@@ -12,9 +12,10 @@ public class ClassInfoMapperTest {
 	private ClassInfoMapper classInfoMapper = MybatisUtil.getSqlSession().getMapper(ClassInfoMapper.class);
 
 	@Test
-	public void insertTest(ClassInfo classId) {
+	public void insertTest() {
 		ClassInfo classInfo = ClassInfo.builder().classId(9988L).businessId(9999L).categoryId(9999L).title("테스트")
-				.description("테스트용").price(5000).duration(100).build();
+				.description("테스트용")
+				.price(5000).duration(100).build();
 		log.info("{}", classInfo);
 		classInfoMapper.insert(classInfo);
 
