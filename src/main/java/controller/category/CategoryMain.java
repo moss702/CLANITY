2070ca291bad7c3c73c9fad7410f9943d9292da5
sr0116 <kysy0116@gmail.com>
2025-07-07 10:45:59 +0900

    카테고리 카드 이미지 추가

--- src/main/java/controller/category/CategoryMain.java
+++ src/main/java/controller/category/CategoryMain.java
@@ -1,6 +1,7 @@
 package controller.category;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,10 +9,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import domain.onedayClass.OnedayClass;
+import service.ClassService;
+
 @WebServlet("/categoryMain")
 public class CategoryMain extends HttpServlet  {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		ClassService service = new ClassService();
+
+		List<OnedayClass> list = service.card();
+		req.setAttribute("cardlist", list);
+//	
 		 req.getRequestDispatcher("/WEB-INF/views/category/categoryMain.jsp").forward(req, resp);
 	}
 
