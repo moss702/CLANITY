9e885ab7ebb68fda9650bc06de5418148f4a9867
luminous7777 <tiger2ys98@gmail.com>
2025-07-09 12:00:28 +0900

    contextDestroyed 리스너에 추가, 서버 실행시 메모리 누수 해결

--- src/main/java/listener/ClassSocialingCategoryListener.java
+++ src/main/java/listener/ClassSocialingCategoryListener.java
@@ -44,4 +44,15 @@ public class ClassSocialingCategoryListener implements ServletContextListener {
 		log.info("ClassSocialingCategory 초기화 완료");
 		log.info("[Listener] 리스트 사이즈: {}, JSON 길이: {} bytes", list.size(), json.length());
 	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		log.info("[Listener] contextDestroyed() 호출 - 종료 정리 작업 시작");
+		ServletContext sc = sce.getServletContext();
+		sc.removeAttribute("csc");
+		sc.removeAttribute("cscJson");
+		log.info("[Listener] contextDestroyed() 종료 - 속성 제거 및 정리 완료");
+	}
+	
+	
 }
