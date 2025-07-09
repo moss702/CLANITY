e0e050b6b0121745a234f8c62a54522b2492ec45
sr0116 <kysy0116@gmail.com>
2025-07-09 18:10:20 +0900

    오류 제거(주석 처리)

--- src/test/java/mapper/ClassInfoMapperTest.java
+++ src/test/java/mapper/ClassInfoMapperTest.java
@@ -1,37 +1,30 @@
 package mapper;
 
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.util.List;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 import com.google.gson.GsonBuilder;
-
-import domain.onedayClass.ClassOpen;
-import domain.onedayClass.OnedayClass;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import util.MybatisUtil;
 
 @Slf4j
 public class ClassInfoMapperTest {
 
-    private ClassSocialingCategoryMapper mapper = MybatisUtil.getSqlSession().getMapper(ClassSocialingCategoryMapper.class);
+    private final ClassSocialingCategoryMapper mapper = MybatisUtil.getSqlSession().getMapper(ClassSocialingCategoryMapper.class);
 
 
 // json 리스너 관련 테스트
 
-//    @Test
-//    @DisplayName("클래스 지역 조회 (JSON 출력)")
-//    public void classTest2() {
-//        String json = new GsonBuilder()
-//            .setPrettyPrinting()
-//            .create()
-//            .toJson(mapper.listRegion());
-//        log.info(json);
-//    }
+    @Test
+    @DisplayName("클래스 지역 조회 (JSON 출력)")
+    public void classTest2() {
+        String json = new GsonBuilder()
+            .setPrettyPrinting()
+            .create()
+            .toJson(mapper.listRegion());
+        log.info(json);
+    }
 //    @Test
 //    @DisplayName("클래스 지역 조회")
 //    public void regionTest() {
@@ -54,7 +47,7 @@ public class ClassInfoMapperTest {
   
     // 클래스 리스트 목록 조회 테스트 (일단 전체 조회 먼저 하고) 카테고리 별 목록 조회
 
-    ClassInfoMapper classInfoMapper = MybatisUtil.getSqlSession().getMapper(ClassInfoMapper.class);
+//    ClassInfoMapper classInfoMapper = MybatisUtil.getSqlSession().getMapper(ClassInfoMapper.class);
 
 //    @Test
 //    @DisplayName("클래스 카테고리 조회")
